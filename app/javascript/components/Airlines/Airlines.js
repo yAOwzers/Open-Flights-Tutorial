fc103459b7605d2689a2d1f18a3824c97e64c580
Joel <jo.neoyj@gmail.com>
2020-12-25 16:31:33 +0800

    Added Styled Components and did up styling

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,13 +1,39 @@
 import React, { useState, useEffect, Fragment } from 'react'
 import axios from 'axios'
+import Airline from './Airline'
+import styled from 'styled-components'
+
+const Home = styled.div`
+    text-align: center;
+    max-width: 1200 px;
+    margin-left: auto;
+    margin-right: auto;
+`
+
+const Header = styled.div`
+    padding: 100px 100px 10px 100px;
+
+    h1 {
+        font-size: 42px;
+    }
+`
+
+const Subheader = styled.div`
+    font-weight: 300;
+    font-size: 26px;
+`
+
+const Grid = styled.div`
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-gap: 20px;
+    padding: 20px;
+`
 
 const Airlines = () => {
     const [airlines, setAirlines] = useState([])
 
     useEffect(() => {
-        //Get all of our airlines from our API
-        //Update airlines in our state
-
         axios.get('/api/v1/airlines.json')
         .then( resp => {
             setAirlines(resp.data.data)
@@ -16,22 +42,28 @@ const Airlines = () => {
     }, [airlines.length])
 
 
-    const list = airlines.map( item => {
-        return (<li key={item.attributes.name}>{item.attributes.name}</li>)
+    const grid = airlines.map( item => {
+        return (
+            <Airline 
+                key={item.attributes.name}
+                attributes={item.attributes}
+
+            />
+        )
     })
 
     return (
-        <Fragment>
-        <div className="home">
-            <div className="header">
+
+        <Home>
+            <Header>
                 <h1>OpenFlights</h1>
-                <div className="sbheader"> Honest, unbiased airline reviews.</div>
-            </div>
-            <div className="grid">
-                <ul>{list}</ul>
-            </div>
-        </div>
-        </Fragment>
+                <Subheader> Honest, unbiased airline reviews.</Subheader>
+            </Header>
+            <Grid>
+                {grid}
+            </Grid>
+        </Home>
+
     )
 }
 
