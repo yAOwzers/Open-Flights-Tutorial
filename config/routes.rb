b01731ede3b84283b35bb7fe1fb066debd000cb8
Joel <jo.neoyj@gmail.com>
2020-12-25 15:00:04 +0800

    created control and tested successfully the API

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :airlines, param :slug
-      resources :reviews, only [:create, :destroy]
+      resources :airlines, param: :slug
+      resources :reviews, only: [:create, :destroy]
     end
   end
 
-  get '*path', to: 'pages#index', via :all
+  get '*path', to: 'pages#index', via: :all
 end
